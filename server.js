6b76014789e2c4a66f2093f5361a6962f8dda898
Adam Pinner <hooklock@gmail.com>
2016-06-16 15:59:24 +0100

    server.js updated

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var app = express();
-var path = require('path')
+var path = require('path');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/bucket_list_app';
+var url = 'mongodb://localhost:27017/a2zSolarSystem';
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.get('/bucketList', function (req, res) {
+app.get('/solarSystem', function (req, res) {
   MongoClient.connect(url, function(err, db) {
-    var collection = db.collection('bucket_countries');
+    var collection = db.collection('planets');
     collection.find({}).toArray(function(err, docs) {
       res.json(docs);
       db.close();
@@ -21,15 +21,15 @@ app.get('/bucketList', function (req, res) {
   });
 });
 
-app.post('/bucketList', function(req,res){
-  console.log('body', req.body)
+app.post('/solarSystem', function(req,res){
+  console.log('body', req.body);
   MongoClient.connect(url, function(err, db) {
-    var collection = db.collection('bucket_countries');
-    collection.insert(req.body.country)
-    res.status(200).end()
+    var collection = db.collection('planets');
+    collection.insert(req.body.country);
+    res.status(200).end();
     db.close();
   });
-})
+});
 
 app.use(express.static('client/build'));
 
