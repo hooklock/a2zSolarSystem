fa3eaad5bda0021f3f0aeb37fd50859233e3abb3
Adam Pinner <hooklock@gmail.com>
2016-06-18 16:33:17 +0100

    planets to Jupiter plus 63 moons

---
+++ mongo.js
@@ -0,0 +1,341 @@
+db.solarSystem.insert({
+    name: "Solar System",
+    sun: "Sol",
+    planets:[
+      {
+        name: "Mercury",
+        earthDistance: 77000000 ,
+        sunDistance: 57910000,
+        orbitalPeriod: 88,
+        diameter: 4879,
+        gravity: 3.7,
+        type: "Terrestrial",
+        mass: "3.285e23kg",
+        dayLength: "58d 15h 30m",
+        axisAngle: 2.11
+      },
+      {
+        name: "Venus",
+        earthDistance: 38000000 ,
+        sunDistance: 108200000,
+        orbitalPeriod: 225,
+        diameter: 12104,
+        gravity: 8.87,
+        type: "Terrestrial",
+        mass: "4.867e24kg",
+        dayLength: "116d 18h 0m",
+        axisAngle: 177.4
+      },
+      {
+        name: "Earth",
+        earthDistance: 0 ,
+        sunDistance: 149600000,
+        orbitalPeriod: 365,
+        diameter: 12742,
+        gravity: 9.807,
+        type: "Terrestrial",
+        mass: "5.972e24kg",
+        dayLength: "0d 24h 0m",
+        axisAngle: 23.5,
+        moons:[
+          {
+            name: "Moon",
+            diameter: 3474
+          }
+        ]
+      },
+      {
+        name: "Mars",
+        earthDistance: 225000000 ,
+        sunDistance: 227900000,
+        orbitalPeriod: 687,
+        diameter: 6779,
+        gravity: 3.711,
+        type: "Terrestrial",
+        mass: "6.39e23kg",
+        dayLength: "1d 0h 40m",
+        axisAngle: 25,
+        moons:[
+          {
+            name: "Phobos",
+            diameter: 22.2
+          },
+          {
+            name: "Demios",
+            diameter: 12.6
+          }
+        ]
+      },
+      {
+        name: "Jupiter",
+        earthDistance: 588000000 ,
+        sunDistance: 778500000,
+        orbitalPeriod: 4380,
+        diameter: 139822,
+        gravity: 24.79,
+        type: "Gas",
+        mass: "1.898e27kg",
+        dayLength: "0d 9h 56m",
+        axisAngle: 3.13,
+        moons:[
+          {
+            name: "Metis",
+            diameter: 45
+          },
+          {
+            name: "Adrastea",
+            diameter: 17
+          },
+          {
+            name: "Amalthea",
+            diameter: 175
+          },
+          {
+            name: "Thebe",
+            diameter: 100
+          },
+          {
+            name: "Io",
+            diameter: 3643
+          },
+          {
+            name: "Europa",
+            diameter: 3121.6
+          },
+          {
+            name: "Ganymede",
+            diameter: 5262.4
+          },
+          {
+            name: "Callisto",
+            diameter: 4820.6
+          },
+          {
+            name: "Themisto",
+            diameter: 8
+          },
+          {
+            name: "Leda",
+            diameter: 16
+          },
+          {
+            name: "Himalia",
+            diameter: 170
+          },
+          {
+            name: "Lysithea",
+            diameter: 36
+          },
+          {
+            name: "Elara",
+            diameter: 86
+          },
+          {
+            name: "S/2000 J 11",
+            diameter: 4
+          },
+          {
+            name: "Carpo",
+            diameter: 3
+          },
+          {
+            name: "S/2003 J 12",
+            diameter: 1
+          },
+          {
+            name: "Euporie",
+            diameter: 2
+          },
+          {
+            name: "S/2003 J 3",
+            diameter: 2
+          },
+          {
+            name: "S/2003 J 18",
+            diameter: 2
+          },
+          {
+            name: "Thelxinoe",
+            diameter: 2
+          },
+          {
+            name: "Euanthe",
+            diameter: 3
+          },
+          {
+            name: "Helike",
+            diameter: 4
+          },
+          {
+            name: "Orthosie",
+            diameter: 2
+          },
+          {
+            name: "Iocaste",
+            diameter: 5
+          },
+          {
+            name: "S/2003 J 16",
+            diameter: 2
+          },
+          {
+            name: "Praxidike",
+            diameter: 7
+          },
+          {
+            name: "Harpalyke",
+            diameter: 4
+          },
+          {
+            name: "Mneme",
+            diameter: 2
+          },
+          {
+            name: "Hermippe",
+            diameter: 4
+          },
+          {
+            name: "Thyone",
+            diameter: 4
+          },
+          {
+            name: "Ananke",
+            diameter: 28
+          },
+          {
+            name: "Herse",
+            diameter: 2
+          },
+          {
+            name: "Aitne",
+            diameter: 3
+          },
+          {
+            name: "Kale",
+            diameter: 2
+          },
+          {
+            name: "Taygete",
+            diameter: 5
+          },
+          {
+            name: "S/2003 J 19",
+            diameter: 2
+          },
+          {
+            name: "Chaldene",
+            diameter: 4
+          },
+          {
+            name: "S/2003 J 15",
+            diameter: 2
+          },
+          {
+            name: "S/2003 J 10",
+            diameter: 2
+          },
+          {
+            name: "S/2003 J 23",
+            diameter: 2
+          },
+          {
+            name: "Erinome",
+            diameter: 3
+          },
+          {
+            name: "Aoede",
+            diameter: 4
+          },
+          {
+            name: "Kallichore",
+            diameter: 2
+          },
+          {
+            name: "Kalyke",
+            diameter: 5
+          },
+          {
+            name: "Carme",
+            diameter: 46
+          },
+          {
+            name: "Callirrhoe",
+            diameter: 9
+          },
+          {
+            name: "Eurydome",
+            diameter: 3
+          },
+          {
+            name: "Pasithee",
+            diameter: 2
+          },
+          {
+            name: "Kore",
+            diameter: 2
+          },
+          {
+            name: "Cyllene",
+            diameter: 2
+          },
+          {
+            name: "Eukelade",
+            diameter: 4
+          },
+          {
+            name: "S/2003 J 4",
+            diameter: 2
+          },
+          {
+            name: "Pasiphaë",
+            diameter: 60
+          },
+          {
+            name: "Hegemone",
+            diameter: 3
+          },
+          {
+            name: "Arche",
+            diameter: 3
+          },
+          {
+            name: "Isonoe",
+            diameter: 4
+          },
+          {
+            name: "S/2003 J 9",
+            diameter: 1
+          },
+          {
+            name: "S/2003 J 5",
+            diameter: 4
+          },
+          {
+            name: "Sinope",
+            diameter: 38
+          },
+          {
+            name: "Sponde",
+            diameter: 2
+          },
+          {
+            name: "Autonoe",
+            diameter: 4
+          },
+          {
+            name: "Megaclite",
+            diameter: 5
+          },
+          {
+            name: "	S/2003 J 2",
+            diameter: 2
+          }
+        ]
+      }
+
+    ]
+
+
+
+  }
+);
