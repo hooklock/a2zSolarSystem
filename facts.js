189d97af0f239c107c32e6ea304fa6a2e696cdfe
Adam Pinner <hooklock@gmail.com>
2016-06-19 13:57:43 +0100

    mercury and some venus facts added

--- facts.js
+++ facts.js
@@ -25,12 +25,85 @@ db.solarSystem.insert(
     fact: "The inner planets (also known as terrestrial planets) are smaller and made mostly of rock and metal. The outer planets (also known as gas giants) are much larger and made mostly of hydrogen, helium and other gases."
   },
   {
+    type: "System",
+    name: "Solar System",
+    fact: "The rotation of a Planet on its axis is what makes a day and the orbit of the planet around the Sun is what makes its year"
+  },
+  {
     type: "Sun",
     name: "Sol",
     fact: "The Sun contains 99.86 percent of the Solar System's known mass, with Jupiter and Saturn making up making up most of the rest. The small inner planets which include Mercury, Venus, Earth and Mars make up a very small percentage of the Solar System’s mass."
   },
   {
-    
+    type: "Moon",
+    name: "Moon",
+    fact: "The phases of the Moon are: New Moon, Crescent, First Quarter, Waxing Gibbous, Full Moon, Waning Gibbous, Last Quarter, Crescent, New Moon."
+  },
+  {
+    type: "Moon",
+    name: "Moon",
+    fact: "The Earth’s tides are largely caused by the gravitational pull of the Moon."
+  },
+  {
+    type: "Moon",
+    name: "Moon",
+    fact: "The effect of gravity is only about one fifth (17%) as strong on the surface of the Moon compared to the strength of gravity on the surface of the Earth."
+  },
+  {
+    type: "Moon",
+    name: "Moon",
+    fact: "The Moon rotates on its axis in around the same length of time it takes to orbit the Earth. This means that from Earth we only ever see around 60% of its surface (50% at any one time)."
+  },
+  {
+    type: "Moon",
+    name: "Moon",
+    fact: "Mons Huygens is the tallest mountain on the Moon, it is 4700 metres tall, just over half the height of Mt Everest (8848m)."
+  },
+  {
+    type: "Moon",
+    name: "Moon",
+    fact: "The Moon is the fifth largest moon in the Solar System, orbits the Earth every 27.3 days and is about 385000 kilometres from Earth."
+  },
+  {
+    type: "Planet",
+    name: "Mercury",
+    fact: "There are no Aliens on Mercury. They all left, came to earth and settled in France."
+  },
+  {
+    type: "Planet",
+    name: "Mercury",
+    fact: "Known as the Water Star by the Ancient Chinese. Despite being the closest planet to the sun and one of the hottest places in the solar system, it in fact is subject to enormous temperature fluctuations. It has no atmosphere to retain heat so Mercury’s poles and the nightside of the planet plunge well below even the coldest temperatures recorded on Earth. With some regions never getting above -170°C, conditions are perfect for ice to form. Mercury’s regolith is home to possibly a trillion tonnes of ice, which would make it one of the wettest places in the solar system."
+  },
+  {
+    type: "Planet",
+    name: "Mercury",
+    fact: "It contains the largest man made crater in space. NASA’s MESSENGER probe was orbiting the planet for 6 years and ran out of fuel in 2014. It could no longer correct its course and as its orbit decayed, it got closer to the planet, and much faster. The resulting crash occurred at over 8000mph and left a crater over 50ft wide, easily the biggest man made crater anywhere in the universe. Apart from Earth!"
+  },
+  {
+    type: "Planet",
+    name: "Mercury",
+    fact: "Mercury's day is longer than its year. Because Mercury orbits so close to the sun it rotates three times on its axis every two years. This makes each 'day' (as in sunrise to sunset and back to sunrise) on Mercury last two of its years. It’s a weird day too - the sun would rise, then go backwards in the sky and set, then rise again before finally setting a year later. The next year, the sun would move in the opposite direction. This is why there the aliens all left, they could not make a suitable calendar to tell what day it was."
+  },
+  {
+    type: "Planet",
+    name: "Mercury",
+    fact: "Mercury smells like wet metallic bottom burps. Mercury’s small size means it has no permanent atmosphere - its gravity is too weak to hold onto any gas in the wake of the strong solar wind. However, it does have a remarkably strong magnetic field which means it keeps a hold of whatever ions come its way. Planets, like Earth, are nice and oxygen filled whereas Mercury’s atmosphere contains things planets normally don’t hang on to. There are a lot of ions in that part of space like magnesium, calcium, sodium, as well as unusual ions of water and hydrogen sulphide."
+  },
+  {
+    type: "Planet",
+    name: "Venus",
+    fact: "There are no Aliens on Venus, they all left and settled in Scotland. In the same neighbourhood of the solar system as Earth it is the closest planet to us and is nearly the same size and has nearly the same gravity. It has mountains and valleys from tectonic activity and a complex weather system in its thick atmosphere. By all measures, it is our twin, our evil twin. Venus is the hottest place in the solar system (aside from the sun), with punishing 450°C temperatures, clouds that consists of sulphuric acid which block out the sun, rain and snow made from toxic lead compounds, and an atmospheric pressure nearly a hundred times that of Earth. The effect would be like being boiled alive while being hugged by the Incredible Hulk while your skin dissolves. The surface is so horrible that for years, the probes we sent there were crushed, then melted. Venus doesn’t tilt on its axis, so it doesn’t have seasons and every day is the hottest day. Still, the upper clouds are rather pleasant, as the atmosphere is so dense, oxygen would float on Venus just as helium does on Earth so perhaps cities in the clouds are an option for the future?"
+  },
+  {
+    type: "",
+    name: "",
+    fact: ""
   }
+  {
+    type: "",
+    name: "",
+    fact: ""
+  }
+
 
 );
