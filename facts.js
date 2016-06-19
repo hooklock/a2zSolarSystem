3120f60cbf44ec7184d5e90df95642a3d699b303
Adam Pinner <hooklock@gmail.com>
2016-06-19 11:40:03 +0100

    fact.js for DB first 6 facts added

---
+++ facts.js
@@ -0,0 +1,36 @@
+db.solarSystem.insert(
+  {
+    type: "Planet",
+    name: "Mercury",
+    fact: "It would take you 142 years to ride a bike at 40km/h from Mercury to Venus."
+  },
+  {
+    type: "System",
+    name: "Galaxy",
+    fact: "Galaxy is dervived from the greek galaxias meaning 'milky' in reference to the 'Milky Way'."
+  },
+  {
+    type: "System",
+    name: "Solar System",
+    fact: "The Solar System formed around 4.6 billion years ago."
+  },
+  {
+    type: "System",
+    name: "Solar System",
+    fact: "There are eight planets in the Solar System. The four inner planets are Mercury, Venus, Earth and Mars while the four outer planets are Jupiter, Saturn, Uranus and Neptune."
+  },
+  {
+    type: "System",
+    name: "Solar System",
+    fact: "The inner planets (also known as terrestrial planets) are smaller and made mostly of rock and metal. The outer planets (also known as gas giants) are much larger and made mostly of hydrogen, helium and other gases."
+  },
+  {
+    type: "Sun",
+    name: "Sol",
+    fact: "The Sun contains 99.86 percent of the Solar System's known mass, with Jupiter and Saturn making up making up most of the rest. The small inner planets which include Mercury, Venus, Earth and Mars make up a very small percentage of the Solar System’s mass."
+  },
+  {
+    
+  }
+
+);
